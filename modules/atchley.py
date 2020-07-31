a83d617e427164f1f50cc6e7b464af7250436389
christellevs <christelle.marie@gmail.com>
2020-07-31 22:22:17 +0100

    added main as .ipynb

--- modules/atchley.py
+++ modules/atchley.py
@@ -5,9 +5,58 @@
 
 class Atchley:
     
-    def __init__():
+    def __init__(self):
         
         
+    def to_dict(self):
+        return 
         
-    def to_dict():
+
+    def _get_atchley_values_list(self, aminos:List[str], idx:int) -> List[int]:
+        """
+        Returns all atchley values in a list for a specific amino acid.
+        """
         
+        return [float(cfg.DICT_ATCHLEY.get(i)[idx]) for i in aminos]
+
+
+    def _get_atchley_values_raw(self, sequence, seq_dict_r):
+        """
+        Returns raw atchley values in a dictionary.
+        """
+        seq_dict_r['f1'] = get_atchley_values_list(sequence, 0)
+        seq_dict_r['f2'] = get_atchley_values_list(sequence, 1)
+        seq_dict_r['f3'] = get_atchley_values_list(sequence, 2)
+        seq_dict_r['f4'] = get_atchley_values_list(sequence, 3)
+        seq_dict_r['f5'] = get_atchley_values_list(sequence, 4)
+
+    # -----------------------------------------------
+
+    def _get_change_list(self, atchley_list):
+        """
+        Calculates sequential change for single atchley value.
+        """
+        atchley_list.insert(0, 0)
+        change_list = [i for i in (np.diff(atchley_list))]
+        atchley_list.pop(0)
+        return change_list
+
+
+    def get_atchley_diff(self, seq_dict_r, seq_dict_d):
+        """
+        Returns the sequential change for each atchley value as a dictionary.
+        """
+        seq_dict_d['f1_d'] = get_change_list(seq_dict_r.get('f1'))
+        seq_dict_d['f2_d'] = get_change_list(seq_dict_r.get('f2'))
+        seq_dict_d['f3_d'] = get_change_list(seq_dict_r.get('f3'))
+        seq_dict_d['f4_d'] = get_change_list(seq_dict_r.get('f4'))
+        seq_dict_d['f5_d'] = get_change_list(seq_dict_r.get('f5'))
+    
+    
+    def append_atchley_values(self, proteins_list):
+        """
+        Appends the atchley values to the dictionary of a ProteinSequence object.
+        """
+        for protein in proteins_list:
+            get_atchley_values_raw(protein.sequence, protein.seq_dict_raw)
+            get_atchley_diff(protein.seq_dict_raw, protein.seq_dict_diff)
--- modules/helper_funcs.py
+++ modules/helper_funcs.py
@@ -19,8 +19,9 @@ from typing import List, Dict
 import config as cfg
 import protein as prot
 
-# COMMON FUNCIONS
 # -----------------------------------------------------------------------------
+
+# FILE RELATED FUNCTIONS
 
 def pickle_method(fname:str, method:str, context=''):
   """
@@ -63,7 +64,7 @@ def split_seq(sequence:str) -> List[str]:
 
 
 # PRE-PROCESSING FUNCIONS
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------
 
 def crop_sequences(proteins: prot.Protein) -> List[prot.Protein]:
   """
@@ -83,95 +84,8 @@ def parse_fasta(path_fasta:str, is_toxic:int) -> List[str]:
         sequences.append( prot.Protein(title.split(None, 1)[0],
                                         is_toxic, len(sequence), split_seq(sequence)) )
   return sequences
-
-
-# ATHCLEY VALUES
-# ---------------------------------------
-
-def get_atchley_values_list(aminos:List[str], idx:int) -> List[int]:
-  """
-  Returns all atchley values in a list for a specific amino acid.
-  """
-  return [float(cfg.DICT_ATCHLEY.get(i)[idx]) for i in aminos]
-
-
-def get_atchley_values_raw(sequence, seq_dict_r):
-  """
-  Returns raw atchley values in a dictionary.
-  """
-  seq_dict_r['f1'] = get_atchley_values_list(sequence, 0)
-  seq_dict_r['f2'] = get_atchley_values_list(sequence, 1)
-  seq_dict_r['f3'] = get_atchley_values_list(sequence, 2)
-  seq_dict_r['f4'] = get_atchley_values_list(sequence, 3)
-  seq_dict_r['f5'] = get_atchley_values_list(sequence, 4)
-
-# -----------------------------------------------
-
-def get_change_list(atchley_list):
-  """
-  Calculates sequential change for single atchley value.
-  """
-  atchley_list.insert(0, 0)
-  change_list = [i for i in (np.diff(atchley_list))]
-  atchley_list.pop(0)
-  return change_list
-
-
-def get_atchley_diff(seq_dict_r, seq_dict_d):
-  """
-  Returns the sequential change for each atchley value as a dictionary.
-  """
-  seq_dict_d['f1_d'] = get_change_list(seq_dict_r.get('f1'))
-  seq_dict_d['f2_d'] = get_change_list(seq_dict_r.get('f2'))
-  seq_dict_d['f3_d'] = get_change_list(seq_dict_r.get('f3'))
-  seq_dict_d['f4_d'] = get_change_list(seq_dict_r.get('f4'))
-  seq_dict_d['f5_d'] = get_change_list(seq_dict_r.get('f5'))
-  
-  
-def append_atchley_values(proteins_list):
-  """
-  Appends the atchley values to the dictionary of a ProteinSequence object.
-  """
-  for protein in proteins_list:
-    get_atchley_values_raw(protein.sequence, protein.seq_dict_raw)
-    get_atchley_diff(protein.seq_dict_raw, protein.seq_dict_diff)
     
 
-# MATRICES VALUES
-# ---------------------------------------
-
-def get_matrix_values(seq_dict):
-  """
-  Returns matrix from input dictionary.
-  """
-  return np.array([seq_dict[i] for i in seq_dict.keys()])
-
-
-def update_matrices(protein_seq_list):
-  """
-  Updates both matrices.
-  """
-  for protein in protein_seq_list:
-    protein.matrix_raw = get_matrix_values(protein.seq_dict_raw)
-    protein.matrix_diff = get_matrix_values(protein.seq_dict_diff)
-    
-def append_proteins(proteins):
-  """
-  Appends Atchley values to the Protein object matrices.
-  """
-  append_atchley_values(proteins)
-  update_matrices(proteins)
-  pickle_method(cfg.f_train_proteins, 'wb', proteins)
-  
-
-def proteins_to_df(proteins:prot.Protein) -> pd.DataFrame:
-  """
-  Returns a DataFrame from a list of proteins.
-  """
-  df = pd.DataFrame.from_records([p.to_dict() for p in proteins])
-  pickle_method(cfg.f_train_df, 'wb', df)
-  return df
-  
 
 # SPLTITING FUNCIONS
 # -----------------------------------------------------------------------------
