7591837ceaa5697205091ba45a4d9cfc94f8a198
christellevs <christelle.marie@gmail.com>
2020-08-09 19:30:46 +0100

    visual funcs updated

--- visual_funcs.py
+++ visual_funcs.py
@@ -9,7 +9,7 @@ from textwrap import wrap
 # -----------------------------------------------------------------------------
 # GRAPH FUNCTIONS
 
-def dist_graph(df, x_dim, y_dim, x_axis_col, title, x_label, y_label, colour, bins):
+def dist_graph_df(df, x_dim, y_dim, x_axis_col, title, x_label, y_label, colour, bins):
   """
   Creates a distribution graph.
   """
@@ -17,6 +17,20 @@ def dist_graph(df, x_dim, y_dim, x_axis_col, title, x_label, y_label, colour, bi
   fig = plt.subplots(figsize=(x_dim, y_dim))
   dist = np.asarray(df[[x_axis_col]].values.tolist()).ravel()
   ax = sns.distplot(df[x_axis_col],
+                       bins=bins, kde=True,
+                       kde_kws={"color": "k", "lw": 2, "label": "KDE"},
+                       color=colour)
+  ax.set_title(title, fontsize=26)
+  ax.set_xlabel(x_label, fontsize=16)
+  ax.set_ylabel(y_label, fontsize=16)
+  
+  
+# make distribution plot
+def dist_graph(values, x_dim, y_dim, title, x_label, y_label, colour, bins):
+  sns.set(style='whitegrid')
+  fig = plt.subplots(figsize=(x_dim, y_dim))
+  dist = np.asarray(list(values)).ravel()
+  ax = sns.distplot(list(values),
                        bins=bins, kde=True,
                        kde_kws={"color": "k", "lw": 2, "label": "KDE"},
                        color=colour)
